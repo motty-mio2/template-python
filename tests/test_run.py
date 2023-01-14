67ff4d2186b6e378c20a7fcb37d9e1ba3cd43242
motty <motty.mio2@gmail.com>
2023-01-14 23:05:44 +0900

    fix mypy

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,10 +1,10 @@
 from template_python import common as c
 
 
-def test_echo():
+def test_echo() -> None:
     assert c.echo() == "Hello"
 
 
-def test_echo_word():
+def test_echo_word() -> None:
     word: str = "test"
     assert c.echo(txt=word) == word
