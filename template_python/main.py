ce0997df755557fbfbf82237529c69dc4be006f2
motty <motty.mio2@gmail.com>
2023-01-15 22:46:13 +0900

    add fastapi

--- template_python/main.py
+++ template_python/main.py
@@ -1,1 +1,16 @@
-print("src")
+import uvicorn
+from faker import Faker
+from fastapi import FastAPI
+
+
+app = FastAPI()
+fake = Faker("jp-JP")
+
+
+@app.get("/")
+def read_root() -> dict[str, str]:
+    return {"H": fake.name()}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
